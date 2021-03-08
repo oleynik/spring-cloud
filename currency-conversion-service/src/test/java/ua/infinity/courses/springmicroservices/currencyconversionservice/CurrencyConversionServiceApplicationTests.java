071aebd871ca9e87a5aa5b5e925287e3a37fd05e
Oleksandr Oliinyk <alex.p.oliinyk@gmail.com>
2021-03-08 17:00:22 +0200

    Init commit for Currency Conversion service

---
+++ currency-conversion-service/src/test/java/ua/infinity/courses/springmicroservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.infinity.courses.springmicroservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
