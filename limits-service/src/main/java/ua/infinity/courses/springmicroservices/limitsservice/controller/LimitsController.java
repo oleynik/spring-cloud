b0a2253ef2bf03de37fee2195abaf41a6a367940
Oleksandr Oliinyk <alex.p.oliinyk@gmail.com>
2021-03-08 15:20:08 +0200

    Init commit. Implemented LimitsController with Configuration.

---
+++ limits-service/src/main/java/ua/infinity/courses/springmicroservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package ua.infinity.courses.springmicroservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.infinity.courses.springmicroservices.limitsservice.bean.Limits;
+import ua.infinity.courses.springmicroservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
