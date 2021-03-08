d959a1b16bd703a19664aa75cb3d8fc443cfee21
Oleksandr Oliinyk <alex.p.oliinyk@gmail.com>
2021-03-08 17:36:32 +0200

    Implemented integration with Feign Client

--- currency-conversion-service/src/main/java/ua/infinity/courses/springmicroservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/ua/infinity/courses/springmicroservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,9 +1,8 @@
 package ua.infinity.courses.springmicroservices.currencyconversionservice;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,10 +11,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
 
-    @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convert(@PathVariable String from,
                                       @PathVariable String to,
                                       @PathVariable BigDecimal quantity) {
@@ -32,6 +33,18 @@ public class CurrencyConversionController {
         BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
         return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
                 conversionMultiple, quantity.multiply(conversionMultiple),
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment()+ " REST Template");
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion convertFeign(@PathVariable String from,
+                                      @PathVariable String to,
+                                      @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = currencyExchangeProxy.exchange(from, to);
+
+        BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                conversionMultiple, quantity.multiply(conversionMultiple),
+                currencyConversion.getEnvironment() + " Feign");
     }
 }
---
+++ currency-conversion-service/src/main/java/ua/infinity/courses/springmicroservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,12 @@
+package ua.infinity.courses.springmicroservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion exchange(@PathVariable String from, @PathVariable String to);
+}
