4be1545834237c4770283f2e1d1fe85279164b32
Oleksandr Oliinyk <alex.p.oliinyk@gmail.com>
2021-03-08 18:53:48 +0200

    Implemented API Gateway

---
+++ api-gateway/src/test/java/ua/infinity/courses/springmicroservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.infinity.courses.springmicroservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
