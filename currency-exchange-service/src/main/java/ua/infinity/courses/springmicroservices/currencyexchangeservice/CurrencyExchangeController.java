781a4039bd9e32136bf6032911e25d98688769cd
Oleksandr Oliinyk <alex.p.oliinyk@gmail.com>
2021-03-10 00:02:03 +0200

    Migrated to release version of spring boot - 2.4.3. Implemented integration of Currency Exchange, Currency Conversion and API Gateway Services with Zipkin

--- currency-exchange-service/src/main/java/ua/infinity/courses/springmicroservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/ua/infinity/courses/springmicroservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package ua.infinity.courses.springmicroservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -19,6 +23,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange exchange(@PathVariable String from, @PathVariable String to) {
+        logger.info("exchange called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
